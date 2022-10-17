deadf3a6fcd35462b2dec598c0327f4058854521
LiuHaolin <25915043@qq.com>
2022-10-18 00:16:06 +0800

    bash

---
+++ signature_hash_experiment/ecdsa/bash_test.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+# nohup go run ECDSA_experiment.go
+# nohup go run ECDSA_experiment.go
+# nohup go run ECDSA_experiment.go
+
+gnome-terminal -x bash -c "go run ECDSA_experiment.go"
+gnome-terminal -x bash -c "go run ECDSA_experiment.go"
+gnome-terminal -x bash -c "go run ECDSA_experiment.go"
